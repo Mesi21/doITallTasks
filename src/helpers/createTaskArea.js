cbceb9e0bf9430083a7e89a91defe3783cafdb94
Mesi <mse212002@gmail.com>
2021-09-27 16:01:40 +0300

    :sparkles: added task list, helpers and displayTasks functions

---
+++ src/helpers/createTaskArea.js
@@ -0,0 +1,20 @@
+import createEle from "./createEle.js";
+
+const createTaskArea = () => {
+  const taskArea = document.getElementById('main');
+  const header = createEle('div', null, 'header-ele', 'Today\'s todos');
+  const add = createEle('div', null, 'add-new', null);
+  const addNewTodo = createEle('input', null, 'to-add', null, null);
+  const newBtn = createEle('button', null, 'submit', 'Add');
+  const clearAllFinished = createEle('div', null, 'clear', 'Clear all completed');
+  addNewTodo.setAttribute('type', 'text');
+  addNewTodo.setAttribute('placeholder', 'Add to your list');
+  newBtn.setAttribute('type', 'submit');
+  add.appendChild(addNewTodo);
+  add.appendChild(newBtn);
+  taskArea.appendChild(header);
+  taskArea.appendChild(add);
+  return taskArea;
+}
+
+export default createTaskArea;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,48 @@
 import _ from 'lodash';
 import './style.css';
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
+import createEle from './helpers/createEle.js';
+import createTaskArea from './helpers/createTaskArea.js';
 
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello')
+const tasks = [
+  {
+    description: 'task 1',
+    completed: false,
+    index: 0
+  },
+  {
+    description: 'task 2',
+    completed: false,
+    index: 1
+  },
+  {
+    description: 'task 3',
+    completed: false,
+    index: 2
+  },
+]
 
-    return element;
-  }
-  
-  document.body.appendChild(component());
+const displayTasks = () => {
+  const mainArea = createTaskArea();
+  const list = createEle('ul', 'todos', 'list-todos', null);
+  tasks.forEach((task) => {
+    const row = createEle('li', null, `task-${task.index}`, null);
+    const label = createEle('label', null, null, task.description);
+    const input = createEle('input', null, task.index, null);
+    const dots = createEle('i', 'fas fa-ellipsis-v', `dot-${task.index}`, null);
+    input.setAttribute('type', 'checkbox');
+    input.setAttribute('value', task.description);
+    label.htmlFor = `${task.index}`;
+    row.appendChild(label);
+    row.appendChild(input);
+    row.appendChild(dots);
+    list.appendChild(row);
+  });
+  mainArea.appendChild(list);
+  return mainArea;
+};
+
+displayTasks();
