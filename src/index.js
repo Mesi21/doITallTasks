405eb71b31274715af21016261e8cfad13df8680
Mesi <mse212002@gmail.com>
2021-10-07 22:04:01 +0300

    :art: added hardcoded elements to the todo list

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import './style.css';
+
+class Todo {
+  constructor() {
+    this.tasks = [
+      {
+        description: task1,
+        completed: false,
+        index: 1,
+      },
+      {
+        descriptio: task2,
+        completed: false,
+        index: 2,
+      }
+    ]
+  }
+}
