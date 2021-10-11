f550fca56e95484ebba37554c598dcb7af9da88e
Mesi <mse212002@gmail.com>
2021-10-11 22:44:46 +0300

    added tasks array

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import './style.css';
 
-class Todo {
-  constructor() {
-    this.tasks = [
-      {
-        description: task1,
-        completed: false,
-        index: 1,
-      },
-      {
-        descriptio: task2,
-        completed: false,
-        index: 2,
-      }
-    ]
+tasks = [
+  {
+    'description': 'task1',
+    'completed': false,
+    'index': 1
+  },
+  {
+    'description': 'task2',
+    'completed': false,
+    'index': 2
+  },
+  {
+    'description': 'task3',
+    'completed': false,
+    'index': 3
   }
-}
+]
